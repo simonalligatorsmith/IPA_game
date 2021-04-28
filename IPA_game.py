5bbdfe3acbe512efbd1e860a89fe19c0f09fb3db
simonalligatorsmith <56604094+simonalligatorsmith@users.noreply.github.com>
2021-04-27 20:35:39 -0600

    Added all IPA vowels
    Added all the IPA vowels per Wikipedia:
    https://en.wikipedia.org/wiki/International_Phonetic_Alphabet_chart
    
    Some vowels like /ə/ (schwa) that don't have a "roundedness" dimension listed are marked as "unrounded." Ask someone if you should add a "N/A" category for roundedness that just applied to schwa and /ɐ/.
    
    The GUI window closes and reopens for every letter refresh. Is this the way to do it, or should the window just update? Find out how to do this.

--- IPA_game.py
+++ IPA_game.py
@@ -1,6 +1,10 @@
 ### Author = {'Simon_Smith':'https://github.com/simonalligatorsmith'} ###
 
-#Install PySimpleGui if not already installed
+# Thanks to RealPython @ https://realpython.com/pysimplegui-python/
+#   for help getting started with the GUI
+
+import random
+# Install PySimpleGui if not already installed
 try:
     import PySimpleGUI as sg
 
@@ -12,51 +16,78 @@ except ModuleNotFoundError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "PySimpleGui"])
     import PySimpleGUI as sg
 
-import random
-
 
 ### define IPA letters ###
-vowels = {'i': {'closeness': 'close', 'backness': 'front', 'roundedness': 'unrounded'},
-          'y': {'closeness': 'close', 'backness': 'front', 'roundedness': 'rounded'},
-          'ɨ': {'closeness': 'close', 'backness': 'central', 'roundedness': 'unrounded'},
-          'ʉ': {'closeness': 'close', 'backness': 'central', 'roundedness': 'rounded'},
-          'ɯ': {'closeness': 'close', 'backness': 'back', 'roundedness': 'unrounded'},
-          'u': {'closeness': 'close', 'backness': 'back', 'roundedness': 'rounded'},
+vowels = {
+        # Close vowels
+        'i': {'closeness': 'close', 'backness': 'front',    'roundedness': 'unrounded'},
+        'y': {'closeness': 'close', 'backness': 'front',    'roundedness': 'rounded'},
+        'ɨ': {'closeness': 'close', 'backness': 'central',  'roundedness': 'unrounded'},
+        'ʉ': {'closeness': 'close', 'backness': 'central',  'roundedness': 'rounded'},
+        'ɯ': {'closeness': 'close', 'backness': 'back',     'roundedness': 'unrounded'},
+        'u': {'closeness': 'close', 'backness': 'back',     'roundedness': 'rounded'},
 
-          'ɪ': {'closeness': 'near-close', 'backness': 'front', 'roundedness': 'unrounded'},
-          'ʏ': {'closeness': 'near-close', 'backness': 'front', 'roundedness': 'rounded'},
-          'ʊ': {'closeness': 'near-close', 'backness': 'central', 'roundedness': 'rounded'},
+        # Near-close vowels
+        'ɪ': {'closeness': 'near-close', 'backness': 'front',   'roundedness': 'unrounded'},
+        'ʏ': {'closeness': 'near-close', 'backness': 'front',   'roundedness': 'rounded'},
+        'ʊ': {'closeness': 'near-close', 'backness': 'central', 'roundedness': 'rounded'},
 
-          'e': {'closeness': 'close-mid', 'backness': 'front', 'roundedness': 'unrounded'},
-          'ø': {'closeness': 'close-mid', 'backness': 'front', 'roundedness': 'rounded'},
-          'ɘ': {'closeness': 'close-mid', 'backness': 'central', 'roundedness': 'unrounded'},
-          'ɵ': {'closeness': 'close-mid', 'backness': 'central', 'roundedness': 'rounded'},
-          'ɤ': {'closeness': 'close-mid', 'backness': 'back', 'roundedness': 'unrounded'},
-          'o': {'closeness': 'close-mid', 'backness': 'back', 'roundedness': 'rounded'},
+        # Close-mid vowels
+        'e': {'closeness': 'close-mid', 'backness': 'front',    'roundedness': 'unrounded'},
+        'ø': {'closeness': 'close-mid', 'backness': 'front',    'roundedness': 'rounded'},
+        'ɘ': {'closeness': 'close-mid', 'backness': 'central',  'roundedness': 'unrounded'},
+        'ɵ': {'closeness': 'close-mid', 'backness': 'central',  'roundedness': 'rounded'},
+        'ɤ': {'closeness': 'close-mid', 'backness': 'back',     'roundedness': 'unrounded'},
+        'o': {'closeness': 'close-mid', 'backness': 'back',     'roundedness': 'rounded'},
 
-          ### Don't forget mid vowels thru open vowels!!! ###
-          'ə': {'closeness': 'mid', 'backness': 'central', 'roundedness': 'unrounded'}
-          }
-# Template: {'closeness':'','backness':'','roundedness':''},
+        # Mid vowels
+        'e̞': {'closeness': 'mid', 'backness': 'front',   'roundedness': 'unrounded'},
+        'ø̞': {'closeness': 'mid', 'backness': 'front',   'roundedness': 'rounded'},
+        'ə': {'closeness': 'mid', 'backness': 'central', 'roundedness': 'unrounded'},
+        'ɤ̞': {'closeness': 'mid', 'backness': 'back',    'roundedness': 'unrounded'},
+        'o̞': {'closeness': 'mid', 'backness': 'back',    'roundedness': 'rounded'},
 
+        # (Note: the 'ɤ̞' diacritic appears discontiguous, but is ok when displayed in the Times New Roman font)
 
-# Create an event loop
+        # Open-mid vowels
+        'ɛ': {'closeness': 'open-mid', 'backness': 'front',   'roundedness': 'unrounded'},
+        'œ': {'closeness': 'open-mid', 'backness': 'front',   'roundedness': 'rounded'},
+        'ɜ': {'closeness': 'open-mid', 'backness': 'central', 'roundedness': 'unrounded'},
+        'ɞ': {'closeness': 'open-mid', 'backness': 'central', 'roundedness': 'rounded'},
+        'ʌ': {'closeness': 'open-mid', 'backness': 'back',    'roundedness': 'unrounded'},
+        'ɔ': {'closeness': 'open-mid', 'backness': 'back',    'roundedness': 'rounded'},
+
+        # Near-open
+        'æ': {'closeness': 'near-open', 'backness': 'front',    'roundedness': 'unrounded'},
+        'ɐ': {'closeness': 'near-open', 'backness': 'central',  'roundedness': 'unrounded'},
+
+        # Open
+        'a': {'closeness': 'open', 'backness': 'front',   'roundedness': 'unrounded'},
+        'ɶ': {'closeness': 'open', 'backness': 'front',   'roundedness': 'rounded'},
+        'ä': {'closeness': 'open', 'backness': 'central', 'roundedness': 'unrounded'},
+        'ɑ': {'closeness': 'open', 'backness': 'back',    'roundedness': 'unrounded'},
+        'ɒ': {'closeness': 'open', 'backness': 'back',    'roundedness': 'rounded'},
+
+}### End definitions for vowels ###
+
+# Create an event loop for the
 while True:
-    # Choose a random symbol
+
+    # Choose a random symbol from 'vowels'
     IPA_symbol = random.choice(list(vowels.keys()))
     question_text = "What is / " + IPA_symbol + ' /?'
 
     # Create layout of the window (button positioning and such)
     layout = [
-        [sg.Text(question_text, font='Courier 30')],
-        #  [sg.Button("\tu\t",font='Courier 20')],
+        [sg.Text(question_text, font='Times_New_Roman 30')],
+
         [
             sg.Combo(['close', 'near-close', 'close-mid', 'mid', 'open-mid', 'near-open', 'open']),
             sg.Combo(['front', 'central', 'back']),
             sg.Combo(['unrounded', 'rounded'])
         ],
-        [sg.Button("OK", font='Courier 20'), sg.Button("Exit", font='Courier 20')]
 
+        [sg.Button("OK", font='Times_New_Roman 20'), sg.Button("Exit", font='Times_New_Roman 20')]
     ]
 
     # Create the window
@@ -79,6 +110,9 @@ while True:
         vowels[IPA_symbol]['roundedness'], '\n'
     )
 
+    # Close the old window
+    window.close()
+
     # End program if user closes window or
     # presses the Exit button
     if event == "Exit" or event == sg.WIN_CLOSED:
@@ -86,5 +120,3 @@ while True:
     # Create a new question in the window
     elif event == "OK":
         continue
-
-window.close()
